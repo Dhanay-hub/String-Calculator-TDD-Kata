62a4cc0ffe0b086dee6db3e67bbc98c8fcb459aa
Dhanay Mehta <151530596+dhanay-mehta@users.noreply.github.com>
2024-08-23 20:23:43 +0530

    feat: added test case for single number

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const add = require('./tdd');
 test('Should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('Should return the number if length is 1', () => {
+    expect(add("10")).toBe(10);
+});

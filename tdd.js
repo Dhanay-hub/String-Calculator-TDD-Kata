8a50d08374b07a393663d29a6688b513386ea89a
Dhanay Mehta <151530596+dhanay-mehta@users.noreply.github.com>
2024-08-23 20:03:29 +0530

    feat: add validation for 0 length and negative number

--- tdd.js
+++ tdd.js
@@ -1,6 +1,11 @@
 const add = (members)  => {
-
-    return members.split(",").reduce((result, e) => result + +e, 0);
+    if(members.length === 0)
+        return 0;
+    const  numArray = members.split(",");
+    if(numArray.some( x => x < 0 ))
+        throw new Error("Array contains a negative number");
+    return numArray.reduce((result, e) => result + +e, 0);
 }
 
-console.log(add("1,2,3"))
+console.log(add("   "))
+// console.log(add("1,2,3,-4"))

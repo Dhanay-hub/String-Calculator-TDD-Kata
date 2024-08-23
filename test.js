c5bdc074cef04939bc8d2bd6ece7d16c775667dc
Dhanay Mehta <151530596+dhanay-mehta@users.noreply.github.com>
2024-08-23 20:25:21 +0530

    feat: add test case for array of length > 1

--- test.js
+++ test.js
@@ -7,3 +7,7 @@ test('Should return 0 for an empty string', () => {
 test('Should return the number if length is 1', () => {
     expect(add("10")).toBe(10);
 });
+
+test('Should return the sum if length is more than 1', () => {
+    expect(add("1,2,3")).toBe(6);
+});

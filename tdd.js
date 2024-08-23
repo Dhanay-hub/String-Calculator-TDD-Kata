20ff300d82d042d262c3f748a088f42c6a7ab14a
Dhanay Mehta <151530596+dhanay-mehta@users.noreply.github.com>
2024-08-23 20:57:09 +0530

    feat: add logic to accomodate custom delimeter

--- tdd.js
+++ tdd.js
@@ -1,11 +1,20 @@
 const add = (members)  => {
     if(members.length === 0)
         return 0;
-    const defaultDelimiters = /,|\n/;
-    const  numArray = members.split(defaultDelimiters);
+    let defaultDelimiters = /,|\n/;
+    if (members.startsWith("//")) {
+        const fragments = members.split("\n", 2); 
+        // Replace member string string with new string w/o delimeter 
+        members = fragments[1];
+        // Replace default delimeter with the one given
+        defaultDelimiters = new RegExp(fragments[0].slice(2)); 
+    }
+    const numArray = members.split(defaultDelimiters);
     if(numArray.some( x => x < 0 ))
         throw new Error("Array contains a negative number which is not allowed");
     return numArray.reduce((result, e) => result + +e, 0);
 }
+
+console.log(add("//:\n1:2:3"))
 
 module.exports = add;

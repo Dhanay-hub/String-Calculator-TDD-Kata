f5b744e6c6b230ebb76853f16a2f352729a9b853
Dhanay Mehta <151530596+dhanay-mehta@users.noreply.github.com>
2024-08-23 20:09:49 +0530

    feat: add ability to split based on multiple delimeters

--- tdd.js
+++ tdd.js
@@ -1,11 +1,12 @@
 const add = (members)  => {
     if(members.length === 0)
         return 0;
-    const  numArray = members.split(",");
+    const defaultDelimiters = /,|\n/;
+    const  numArray = members.split(defaultDelimiters);
     if(numArray.some( x => x < 0 ))
         throw new Error("Array contains a negative number");
     return numArray.reduce((result, e) => result + +e, 0);
 }
 
-console.log(add("   "))
-// console.log(add("1,2,3,-4"))
+console.log(add("1,2\n3"))
+console.log(add("1\n2\n4"))
